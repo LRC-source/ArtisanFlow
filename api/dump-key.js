75c449e425bb81d1566467ebb6cda70dde5cc0d8
Lola Architect <architect@artisanflow.ai>
2026-09-12 10:18:51 -0400

    Remove dump key

--- api/dump-key.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  res.status(200).json({ 
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
-  });
-}
